f7b048306787aba6402e0e5939e71942c2c800cf
Tucker Elliott <telliott@adobe.com>
2024-04-29 17:00:34 -0400

    update styled blocks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -82,7 +82,7 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
-  buildAutoBlocks(main);
+  // buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
 }
